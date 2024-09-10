a018638f996f282ebd5b84693180078c6e980eff
Alejandro Ahmad <yafarahmad72@gmail.com>
2024-09-10 16:05:49 -0300

    Actualizando Juego en java

--- primera app/src/App.java
+++ primera app/src/App.java
@@ -13,44 +13,52 @@ public class App {
         char[] letrasAdivinadas = new char[palabraSecreta.length()];
 
         for (int i = 0; i < letrasAdivinadas.length; i++) {
-            letrasAdivinadas[i] = '_';
+            letrasAdivinadas[i] = '_'; // Inicializamos el array con '_'
         }
 
         while (!estaAdivinada && intento < intentosMaximos) {
-
-            //el String.valueOf() se lo utiliza cuando se forma una palabra de char
-
-            System.out.println("Palabra a adivinar:" + String.valueOf(letrasAdivinadas) + " (" + (palabraSecreta.length() - letrasRestantes) + " letras restantes)");
-
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " (" + (palabraSecreta.length() - letrasRestantes) + " letras restantes)");
             System.out.println("Introduce una letra: ");
-
             char letra = Character.toLowerCase(scanner.next().charAt(0));
 
             boolean letraCorrecta = false;
+            boolean letraRepetida = false;
 
-            for (int i = 0; i < palabraSecreta.length(); i++) {
-                if(palabraSecreta.charAt(i) == letra){
-                    letrasAdivinadas[i] = letra;
-                    letraCorrecta = true;
-                    letrasRestantes++;
+            // Comprobamos si la letra ya ha sido ingresada
+            for (int i = 0; i < letrasAdivinadas.length; i++) {
+                if (letrasAdivinadas[i] == letra) {
+                    letraRepetida = true;  // Marcamos si la letra ya fue adivinada
+                    break;
                 }
             }
 
-            if(!letraCorrecta){
-                intento++;
-                System.out.println("Incorrecto! Te quedan " + (intentosMaximos - intento) + " intentos");
-                
+            if (!letraRepetida) {  // Si la letra no está repetida, la procesamos
+                for (int i = 0; i < palabraSecreta.length(); i++) {
+                    if (palabraSecreta.charAt(i) == letra) {
+                        letrasAdivinadas[i] = letra;
+                        letraCorrecta = true;
+                        letrasRestantes++;
+                    }
+                }
+
+                if (!letraCorrecta) {
+                    intento++;
+                    System.out.println("Incorrecto! Te quedan " + (intentosMaximos - intento) + " intentos");
+                }
+            } else {
+                System.out.println("¡Ya has ingresado esa letra! Intenta con otra.");
             }
 
-            if(String.valueOf(letrasAdivinadas).equals(palabraSecreta)){
+            if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)) {
                 estaAdivinada = true;
-                System.out.println("Felicidades has adivinado la palabrba secreta!! " + palabraSecreta);
+                System.out.println("¡Felicidades, has adivinado la palabra secreta! " + palabraSecreta);
             }
         }
-        if(!estaAdivinada){
-            System.out.println("Que pena, te has quedado sin intentos. LOSSER");
+
+        if (!estaAdivinada) {
+            System.out.println("Que pena, te has quedado sin intentos. ¡Lo siento!");
         }
-        
+
         scanner.close();
     }
 }
